c8e6138a6dbb2121e3aed678448516ea1397be17
羅仲宏 <chunghung@hungs-MacBook-Pro.local>
2023-04-18 23:33:53 +0800

    datepicker value

---
+++ vue/src/components/calendar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import VueCal from 'vue-cal'
+import 'vue-cal/dist/vuecal.css'
+import { ref } from 'vue';
+//https://github.com/ddmy/vue3-gantt
+const selectedDate = ref(new Date())
+
+
+</script>
+<template>
+    <vue-cal :time="false" @cell-click="selectedDate = $event" :selected-date="selectedDate" hide-weekends
+        class="vuecal--date-picker" />
+    <div>Selected date: '{{ selectedDate.format() }}'</div>
+    <!-- <vue-cal xsmall hide-view-selector click-to-navigate :time="false" active-view="month" :disable-views="['week']"
+                                                                        :disable-days="[
+                                                                            // new Date().subtractDays(2).format(),
+                                                                            new Date('2023-04-30').format(),
+                                                                            // new Date().addDays(2).format()
+                                                                        ]"></vue-cal> -->
+</template>
--- vue/src/views/AboutView.vue
+++ vue/src/views/AboutView.vue
@@ -1,15 +1,8 @@
+<script setup>
+import calendar from '@/components/calendar.vue';
+</script>
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <calendar></calendar>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+
